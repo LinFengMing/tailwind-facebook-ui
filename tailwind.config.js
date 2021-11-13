efddc1cbd417cdd5c0ca896750663c0138248d45
jiro <jiro853141@gmail.com>
2021-11-13 21:20:04 +0800

    feat: 實戰仿Facebook UI Tailwind 主題config配置

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,16 @@ module.exports = {
   mode: 'jit',
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'fb-bg': '#18191a',
+        'fb-header': '#242526',
+        'fb-input': '#3a3b3c',
+        'fb-popover': '#323436',
+        'fb-card': '#242526',
+        'fb': '#2e89ff',
+      }
+    },
   },
   variants: {
     extend: {},
