ac0edfd0daa98b24571e42a432c26c43342297ef
jiro <jiro853141@gmail.com>
2021-11-14 22:12:14 +0800

    feat: 實戰仿Facebook UI，左側資訊欄實作

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import "./index.css";
-
+// Panel
 const plusBtn = document.getElementById('plus-btn')
 const msgBtn = document.getElementById('msg-btn')
 const notificationBtn = document.getElementById('notification-btn')
@@ -66,3 +66,97 @@ notificationPanel.addEventListener('click', (event) => {
 morePanel.addEventListener('click', (event) => {
   event.stopPropagation()
 })
+// 左側相關
+const leftBlock = document.getElementById('left-block')
+
+function renderLeftItem(name, imageUrl) {
+  const item = `
+    <div
+      class="
+        flex
+        items-center
+        justify-items-center
+        w-full
+        p-2
+        mb-1
+        rounded
+        hover:bg-fb-input
+        cursor-pointer
+    "
+    >
+      <div class="w-[32px] overflow-hidden rounded-full mr-4">
+        <img src="${imageUrl}" />
+      </div>
+
+      <p class="text-white text-[.9rem]">${name}</p>
+    </div>
+  `
+
+  return item
+}
+
+function renderLeftBlock() {
+  const leftArr = [
+    {
+      name: 'Jiro',
+      img: 'https://bruce-fe-fb.web.app/image/avator.png',
+    },
+    {
+      name: '活動',
+      img: 'https://bruce-fe-fb.web.app/image/left/activity.svg',
+    },
+    {
+      name: '天氣',
+      img: 'https://bruce-fe-fb.web.app/image/left/cloudy.png',
+    },
+    {
+      name: '災害應變中心',
+      img: 'https://bruce-fe-fb.web.app/image/left/dynamic.svg',
+    },
+    {
+      name: '新冠病毒資訊中心',
+      img: 'https://bruce-fe-fb.web.app/image/left/facemask.svg',
+    },
+    {
+      name: '社團',
+      img: 'https://bruce-fe-fb.web.app/image/left/friend.svg',
+    },
+    {
+      name: '企業管理平台',
+      img: 'https://bruce-fe-fb.web.app/image/left/job.png',
+    },
+    {
+      name: 'Messenger',
+      img: 'https://bruce-fe-fb.web.app/image/left/messenger.svg',
+    },
+    {
+      name: '近期廣告動態',
+      img: 'https://bruce-fe-fb.web.app/image/left/pay.png',
+    },
+    {
+      name: '朋友名單',
+      img: 'https://bruce-fe-fb.web.app/image/left/sale.png',
+    },
+    {
+      name: '最愛',
+      img: 'https://bruce-fe-fb.web.app/image/left/star.svg',
+    },
+    {
+      name: 'Marketplace',
+      img: 'https://bruce-fe-fb.web.app/image/left/store.svg',
+    },
+    {
+      name: 'Watch',
+      img: 'https://bruce-fe-fb.web.app/image/left/watchingTv.svg',
+    },
+  ]
+
+  let htmStr = ''
+  leftArr.forEach(obj => {
+    htmStr += renderLeftItem(obj.name, obj.img)
+  })
+
+  leftBlock.innerHTML = htmStr
+}
+
+renderLeftBlock()
